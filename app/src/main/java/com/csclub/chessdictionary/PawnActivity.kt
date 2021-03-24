8b1b34e47ddd7cb03296bc2eb1ecbc346e0b365f
ajprieger <alexander.prieger@pepperdine.edu>
2021-03-24 13:51:38 -0700

    Initial commit

---
+++ app/src/main/java/com/csclub/chessdictionary/PawnActivity.kt
@@ -0,0 +1,11 @@
+package com.csclub.chessdictionary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PawnActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pawn)
+    }
+}
